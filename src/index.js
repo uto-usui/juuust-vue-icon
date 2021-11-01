bd8178ac668fdb6c93f40da10aec75e8a07f9700
uto-usui <me@uto-usui.com>
2021-11-02 00:44:13 +0900

    Update

---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { default as IconGasStation } from './vue/GasStation.vue';
+export { default as IconLocation } from './vue/location.vue';
+export { default as IconHospital } from './vue/hospital.vue';
+export { default as IconElectricBike } from './vue/ElectricBike.vue';
