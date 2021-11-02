d0b5e3a0e2c0d8295edafd073a4ba6def10e6e1d
GitHub Push Action <push@no-reply.github.com>
2021-11-02 01:38:47 +0000

    GitHub Push

---
+++ src/vue/DoneOutline.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg" :width="size" :height="size" viewBox="0 0 24 24" aria-hidden="true" v-on="$listeners" :fill="color"
+  >
+    <path d="M19.77 5.03l1.4 1.4L8.43 19.17l-5.6-5.6 1.4-1.4 4.2 4.2L19.77 5.03zm0-2.83L8.43 13.54l-4.2-4.2L0 13.57 8.43 22 24 6.43 19.77 2.2z"></path>
+  </svg>
+</template>
+<script>
+  export default {
+    name: "IconDoneOutline",
+    props: {
+      size: {
+        type: Number,
+        default: 16
+      },
+      color: {
+        type: String,
+        default: "currentColor"
+      }
+    }
+  };
+</script>
---
+++ src/vue/hospital.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg" :width="size" :height="size" viewBox="0 0 24 24" aria-hidden="true" v-on="$listeners" :fill="color"
+  >
+    <path d="M19 3H5c-1.1 0-1.99.9-1.99 2L3 19c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-1 11h-4v4h-4v-4H6v-4h4V6h4v4h4v4z"></path>
+  </svg>
+</template>
+<script>
+  export default {
+    name: "IconHospital",
+    props: {
+      size: {
+        type: Number,
+        default: 16
+      },
+      color: {
+        type: String,
+        default: "currentColor"
+      }
+    }
+  };
+</script>
