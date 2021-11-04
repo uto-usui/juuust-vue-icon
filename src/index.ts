254c81d23bff589859cad8bb9a7fae4c07bddd93
uto-usui <me@uto-usui.com>
2021-11-04 15:32:54 +0900

    Remove

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as IconGasStation } from './vue/GasStation.vue';
-export { default as IconLocation } from './vue/location.vue';
-export { default as IconHospital } from './vue/hospital.vue';
-export { default as IconElectricBike } from './vue/ElectricBike.vue';
-export { default as IconSettings } from './vue/Settings.vue';
-export { default as IconPets } from './vue/Pets.vue';
-export { default as IconDoneOutline } from './vue/DoneOutline.vue';
